df44148d1cdb84a8f4cdf68d279c060b87fb4a7d
Tam Pham <tamphamnguyen92@gmail.com>
2020-07-21 01:48:06 +0800

    Updates

---
+++ precache-manifest.67e817a5c2350335442fbaf8b10ebee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c475a268e5534afcefa754c0b6644701",
+    "url": "/jessephamsg/trivia-app/index.html"
+  },
+  {
+    "revision": "244d69da53be3f27e3a9",
+    "url": "/jessephamsg/trivia-app/static/css/main.b62fa111.chunk.css"
+  },
+  {
+    "revision": "4bc53a0f8eb4103f8a98",
+    "url": "/jessephamsg/trivia-app/static/js/2.a191de53.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/jessephamsg/trivia-app/static/js/2.a191de53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244d69da53be3f27e3a9",
+    "url": "/jessephamsg/trivia-app/static/js/main.48156d46.chunk.js"
+  },
+  {
+    "revision": "5a9bfc450acba08c1325",
+    "url": "/jessephamsg/trivia-app/static/js/runtime-main.9086aa38.js"
+  }
+]);
